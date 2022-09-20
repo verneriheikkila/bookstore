f502fee82931fd5419aa5ad5157d93b4233a5801
verneriheikkila <verneri.heikkila@gmail.com>
2022-09-20 21:02:49 +0300

    book entity added

---
+++ src/main/java/fi/SWD4TN020/bookstore/domain/Book.java
@@ -0,0 +1,41 @@
+package fi.SWD4TN020.bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String name, author, yearPublished;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getYearPublished() {
+		return yearPublished;
+	}
+	public void setYearPublished(String yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	
+	
+}
