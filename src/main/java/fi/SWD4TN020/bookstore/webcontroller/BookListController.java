80ba69937ce9996ee1ebe5dcc9848d0d36be4ec7
verneriheikkila <verneri.heikkila@gmail.com>
2022-10-11 15:45:16 +0300

    @PreAuthorize added

--- src/main/java/fi/SWD4TN020/bookstore/webcontroller/BookListController.java
+++ src/main/java/fi/SWD4TN020/bookstore/webcontroller/BookListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +45,7 @@ public class BookListController {
 
 	// Delete book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		brepository.deleteById(bookId);
 		return "redirect:/booklist";
