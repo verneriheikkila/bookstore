9196ec578f5ca00944b37112438a16aa3a14c6e4
verneriheikkila <verneri.heikkila@gmail.com>
2022-10-11 15:51:55 +0300

    last annotation fixed

--- src/main/java/fi/SWD4TN020/bookstore/webcontroller/BookListController.java
+++ src/main/java/fi/SWD4TN020/bookstore/webcontroller/BookListController.java
@@ -45,7 +45,7 @@ public class BookListController {
 
 	// Delete book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	@PreAuthorize("hasAuthority('ADMIN')")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		brepository.deleteById(bookId);
 		return "redirect:/booklist";
